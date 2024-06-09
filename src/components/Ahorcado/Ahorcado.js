a7c3142754d680edef0e1b4b03652820bfb084bd
Brian Carrillo <77851362+briancarrilloo@users.noreply.github.com>
2024-06-09 13:28:45 +0200

    Adde input

--- src/components/Ahorcado/Ahorcado.js
+++ src/components/Ahorcado/Ahorcado.js
@@ -4,6 +4,9 @@ import wordDatabase from "./wordDatabase.json";
 
 const Ahorcado = () => {
     const [currentWord, setCurrentWord] = useState(getRandomWord());
+    const [currentWordArray, setCurrentWordArray] = useState([]);
+    const [revealedLetters, setRevealedLetters] = useState([]);
+    const [inputValue, setInputValue] = useState('');
 
     useEffect(() => {
         initComponent()
@@ -12,7 +15,7 @@ const Ahorcado = () => {
     function initComponent() {
         const randomWord = getRandomWord();
         setCurrentWord(randomWord);
-
+        setCurrentWordArray(randomWord.split(''));
     }
 
     function getRandomWord() {
@@ -21,10 +24,49 @@ const Ahorcado = () => {
         return randomWord.toUpperCase();
     }
 
+    function handleInputChange(event) {
+        let input = event.target.value;
+        if (input.length > 1) {
+            return;
+        }
+        if (!isLetter(input)) {
+            return;
+        }
+        setInputValue(input);
+    }
+
+    function isLetter(string) {
+        return /^[a-zA-Z]*$/.test(string);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newLetter = inputValue.toLowerCase();
+        if (!revealedLetters.includes(newLetter)) {
+            setRevealedLetters([...revealedLetters, newLetter]);
+        }
+        setInputValue('');
+    };
+
     return (
         <div className="ahorcado-container">
             <h1>El juego del ahorcado</h1>
-            <p>La palabra es: {currentWord}</p>
+            {currentWordArray.map((letter, index) => (
+                <p key={index}>{index} - {letter}</p>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Ingrese una letra" />
+                <button type="submit">Enviar</button>
+            </form>
+            <div className='debug'>
+                <p> - - - - Debug - - - - </p>
+                <p>currentWord: {currentWord}</p>
+                <p>revealedLetters: {revealedLetters}</p>
+            </div>
         </div>
     );
 };
