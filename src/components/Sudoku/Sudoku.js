1f4659a4a5a2665a1d6b5d8cbcd32530b79199a9
Brian Carrillo <bcarrillo2302@gmail.com>
2024-06-24 23:45:30 +0200

    Basic sudoku

--- src/components/Sudoku/Sudoku.js
+++ src/components/Sudoku/Sudoku.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ContainerHeader from "../Library/Container";
 import WinLose from "../WinLose/WinLose";
+import './Sudoku.css'
 
 const Sudoku = () => {
     const gameName = "Sudoku";
@@ -11,28 +12,34 @@ const Sudoku = () => {
     const [gameFinished, setGameFinished] = useState(false);
     const [sudoku, setSudoku] = useState([]);
 
-    useEffect(() => {
-        initComponent();
-    }, []);
+    // useEffect(() => {
+    //     initComponent();
+    // }, []);
 
     function initComponent() {
-        let matrix = generateStructure();
-        matrix = generateSudoku(matrix);
-        console.log(matrix);
-        setSudoku(matrix);
+        let emptyMatrix = generateStructure();
+        generateSudoku(emptyMatrix);
     }
 
     function generateSudoku(matrix) {
         let [emRow, emCell] = findEmptyCell(matrix);
-        console.log(emRow);
-        console.log(emCell);
         if (emRow === null) {
+            setSudoku(matrix)
             return true;
         }
-        matrix[emRow][emCell] = 1;
 
-        if (generateSudoku(matrix)) {
-            return true;
+        for (let number = 1; number <= 9; number++) {
+            // TODO: Randomize number selection
+            if (isNumberValid(number, matrix, emRow, emCell)) {
+                console.log("Valid: " + number + emRow + emCell);
+                matrix[emRow][emCell] = number;
+
+                if (generateSudoku(matrix)) {
+                    return true;
+                }
+
+                matrix[emRow][emCell] = 0;
+            }
         }
 
         return false;
@@ -40,21 +47,18 @@ const Sudoku = () => {
 
     function generateStructure() {
         let matrix = [];
-        let emptyRow = [];
-        for (let x = 0; x < width; x++) {
-            emptyRow.push(0);
-        }
-
         for (let y = 0; y < height; y++) {
+            let emptyRow = [];
+            for (let x = 0; x < width; x++) {
+                emptyRow.push(0);
+            }
             matrix.push(emptyRow);
         }
 
-        console.log(matrix);
         return matrix;
     }
 
     function findEmptyCell(matrix) {
-        // console.log(matrix);
         for (let y = 0; y < matrix.length; y++) {
             const row = matrix[y];
             for (let x = 0; x < row.length; x++) {
@@ -67,23 +71,41 @@ const Sudoku = () => {
         return [null, null];
     }
 
-    function isNumberValid() {
 
+    function isNumberValid(number, matrix, row, col) {
+        // Row does not contain number
+        for (let x = 0; x < matrix[row].length; x++) {
+            const cell = matrix[row][x];
+            if (cell === number) {
+                return false;
+            }
+        }
+        // Col does not contain number
+        for (let y = 0; y < matrix[col].length; y++) {
+            const cell = matrix[y][col];
+            if (cell === number) {
+                return false;
+            }
+        }
+        // 3x3 grid does not contain number
+        // TODO
+        return true;
     }
 
     return (
         <div className="container sudoku-container">
             {gameFinished && <WinLose isWin={true} winMessage={winMessage} loseMessage={loseMessage} restartGame={initComponent} />}
             <ContainerHeader title={gameName} restartGame={initComponent} />
-            <button type="button" class="btn btn-primary" onClick={initComponent}>initComponent()</button>
-            {sudoku.map((row, rowIndex) => (
-                <div className="sudoku-row" key={rowIndex}>
-                    {row.map((cell, cellIndex) => (
-                        <div className="sudoku-cell" key={cellIndex}>{cell}</div>
-                    ))}
-                </div>
-            ))}
-
+            <button type="button" className="btn btn-primary" onClick={initComponent}>initComponent()</button>
+            <div className='sudoku'>
+                {sudoku.length > 0 && sudoku.map((row, rowIndex) => (
+                    <div className="sudoku-row" key={rowIndex}>
+                        {row.map((cell, cellIndex) => (
+                            <div className="sudoku-cell" key={cellIndex}>{cell}</div>
+                        ))}
+                    </div>
+                ))}
+            </div>
 
             {/* debug */}
             {/* <div className='sudoku-debug'>
